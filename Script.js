3bec58c2150058db7159650c63672ea106705ae6
mahad100 <mahad991@hotmail.co.uk>
2024-10-31 08:00:00 +0000

    DAY 1: Band Name Generator\nLearning: Printing, String manipulation, Inputs, Variables

--- Script.js
+++
@@ -1,3 +0,0 @@
-document.getElementById("helloButton").addEventListener("click", function() {
-    document.getElementById("message").innerText = "Hello, World!";
-});
---
+++ day_1/task.py
@@ -0,0 +1,3 @@
+city = input('What city did you grow up in?')
+pet = input('What is the name of your pet')
+print (city, pet)
