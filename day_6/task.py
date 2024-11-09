29971e436fa00662c0d51b6a5d60924ed832ca61
mahad100 <mahad991@hotmail.co.uk>
2024-11-09 00:00:00 +0000

    DAY 6: Reeborgs world game\nLearning: Functions!

---
+++ day_6/task.py
@@ -0,0 +1,34 @@
+#Reborgs world game online
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+
+def turn_around():
+    turn_left()
+    turn_left()
+
+
+def movements():
+    move()
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+
+
+movements()
+movements()
+movements()
+movements()
+movements()
+movements()
+
+
+
+
